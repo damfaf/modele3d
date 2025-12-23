1ea8d69c0ac807f4adfbbce6209e495e4330d9c3
Damian <108766397+damfaf@users.noreply.github.com>
2025-12-23 23:21:35 +0100

    Add files via upload

---
+++ pano/data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pokoj1",
+      "name": "pokoj1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 8192,
+      "initialViewParameters": {
+        "yaw": -1.3946557156024966,
+        "pitch": 0.15519992147920192,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.686941694278966,
+          "pitch": 0.205212733499625,
+          "rotation": 4.71238898038469,
+          "target": "1-pokoj2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.631334249566958,
+          "pitch": 0.1450983834699322,
+          "title": "Monitor",
+          "text": "monitor"
+        },
+        {
+          "yaw": -2.1794251557487527,
+          "pitch": -0.12850238985981832,
+          "title": "Lodówka",
+          "text": "lodowka"
+        }
+      ]
+    },
+    {
+      "id": "1-pokoj2",
+      "name": "pokoj2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 8192,
+      "initialViewParameters": {
+        "yaw": 0.37424660063271276,
+        "pitch": 0.06615078620424697,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
